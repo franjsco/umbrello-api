c20d67455e7f24883282ff329366d921899c65c1
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-08-15 23:24:43 +0200

    add server

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import routes from './routes';
+import logger from './logger';
+
+const app = express();
+
+app.use('/api/weather', routes);
+
+app.get('/', (req, res) => {
+  res.send('umbrello-api');
+});
+
+
+app.get('*', (req, res) => {
+  logger.error(`404 ${req.path}`);
+  res.send(404).send();
+});
+
+app.listen(3000, () => {
+  logger.info('Server started');
+});
+
+
+process
+  .on('unhandledRejection', (reason) => logger.error(reason))
+  .on('uncaughtException', (err) => {
+    logger.error(err);
+    process.exit(1);
+  })
+  .on('SIGINT', () => {
+    logger.info('Server stopped');
+    process.exit(0);
+  });
